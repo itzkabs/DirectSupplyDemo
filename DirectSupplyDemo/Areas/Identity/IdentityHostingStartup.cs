228b55ba20d20816e17f6bd1073e501fc93cad01
Kabul Hussain Barbhuiya <kabulbarbhuiya@outlook.com>
2020-05-29 10:36:35 -0500

    Add project files.

---
+++ DirectSupplyDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DirectSupplyDemo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DirectSupplyDemo.Areas.Identity.IdentityHostingStartup))]
+namespace DirectSupplyDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
